480a36bf5e63df94904d4e975f6c2b131bd0e195
TocsaXawer <csabaeror@gmail.com>
2024-02-27 14:47:36 +0100

    Finnally

--- em/Program.cs
+++ em/Program.cs
@@ -5,10 +5,19 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
 using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+
+class ConfigData
+{
+    public string path { get; set; }
+    public string extension { get; set; }
+}
 
 class Program
 {
-    static string DirectoryOpen(string path, string extension)
+    static void DirectoryOpen(string path, string extension)
     {
         Console.WriteLine("kiterjesztes: " + extension);
 
@@ -20,14 +29,11 @@ class Program
             fileList.Add(file.FullName);
         }
 
-        StringBuilder result = new StringBuilder();
-        result.AppendLine("File kiterjesztese " + extension + ":");
+        Console.WriteLine("File kiterjesztese " + extension + ":");
         foreach (string file in fileList)
         {
-            result.AppendLine(file);
+            Console.WriteLine(file);
         }
-
-        return result.ToString();
     }
 
 
@@ -36,8 +42,19 @@ class Program
         Outlook.Application outlookApp = new Outlook.Application();
         Outlook.NameSpace outlookNamespace = outlookApp.GetNamespace("MAPI");
         Outlook.Folder rootFolder = outlookNamespace.Session.DefaultStore.GetRootFolder() as Outlook.Folder;
-        Outlook.Folder targetFolder = rootFolder.Folders.Add("MyFilesFolder", Outlook.OlDefaultFolders.olFolderInbox) as Outlook.Folder;
 
+        // Ellenőrizzük, hogy a célmappa létezik-e, ha nem, létrehozzuk
+        Outlook.Folder targetFolder;
+        try
+        {
+            targetFolder = rootFolder.Folders["em_altal_letrehozva"] as Outlook.Folder;
+        }
+        catch (System.Exception)
+        {
+            targetFolder = rootFolder.Folders.Add("em_altal_letrehozva", Outlook.OlDefaultFolders.olFolderInbox) as Outlook.Folder;
+        }
+
+        // Fájlok feldolgozása és küldése
         for (int i = 0; i < filepaths.Length; i++)
         {
             Console.WriteLine("Processing file: " + filepaths[i]);
@@ -53,16 +70,174 @@ class Program
         outlookApp.Quit();
     }
 
+    static void print_config()
+    {
+            string filePath = @"C:\em\.config.json";
+
+            // Ellenőrizze, hogy a fájl létezik-e
+            if (File.Exists(filePath))
+            {
+                // Olvassa be a JSON tartalmat a fájlból
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deszerializálja a JSON-t egy objektummá
+                ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+                // Most már használhatod a configData objektumot, amely tartalmazza a JSON-ből kiolvasott adatokat
+                Console.WriteLine("A config file tartalma:");
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"path: {configData.path}");
+                Console.WriteLine($"extension: {configData.extension}");
+            }
+            else
+            {
+                Console.WriteLine("A fájl nem létezik: " + filePath);
+            }
+    }
+    static void go()
+    {
+        string filePath = @"C:\em\.config.json";
+        if (File.Exists(filePath))
+        {
+            // Olvassa be a JSON tartalmat a fájlból
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Deszerializálja a JSON-t egy objektummá
+            ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+            // Most már használhatod a configData objektumot, amely tartalmazza a JSON-ből kiolvasott adatokat
+
+            string directoryPath = configData.path;
+            string extension = configData.extension;
+
+            string[] filepaths = Directory.GetFiles(directoryPath, "*" + extension, SearchOption.AllDirectories);
+            SaveToPST(filepaths, "C:\\Output.pst");
+        }
+        else
+        {
+            Console.WriteLine("A fájl nem létezik: " + filePath);
+        }
+    }
+    static void add_extension()
+    {
+        string filePath = @"C:\em\.config.json";
+
+        if (File.Exists(filePath))
+        {
+            // Olvassa be a JSON tartalmat a fájlból
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Deszerializálja a JSON-t egy objektummá
+            ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+            // Kiterjesztes módosítása
+            Console.Write("Kiterjesztes:");
+            string newExtension = Console.ReadLine();
+            configData.extension = newExtension;
+
+            // A fájlba írás előtt JSON formátumra alakítjuk a configData objektumot
+            string updatedJsonContent = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+            // A fájlba írás
+            File.WriteAllText(filePath, updatedJsonContent);
+
+            Console.WriteLine("Kiterjesztés módosítva.");
+        }
+        else
+        {
+            Console.WriteLine("A fájl nem létezik: " + filePath);
+        }
+    }
+
+
+    static void add_spot()
+    {
+        string filePath = @"C:\em\.config.json";
+
+        if (File.Exists(filePath))
+        {
+            // Olvassa be a JSON tartalmat a fájlból
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Deszerializálja a JSON-t egy objektummá
+            ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+            // Path módosítása
+            Console.Write("Mappa Eleresi helye:");
+            string newPath = Console.ReadLine();
+            configData.path = newPath;
+
+            // A fájlba írás előtt JSON formátumra alakítjuk a configData objektumot
+            string updatedJsonContent = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+            // A fájlba írás
+            File.WriteAllText(filePath, updatedJsonContent);
+
+            Console.WriteLine("Mappa sikeresen módosítva.");
+        }
+        else
+        {
+            Console.WriteLine("A fájl nem létezik: " + filePath);
+        }
+    }
+
+    static void CheckAndCreateConfigFile()
+    {
+        string directoryPath = @"C:\em";
+        string filePath = @"C:\em\.config.json";
+
+        // Ellenőrizze, hogy létezik-e a könyvtár
+         if (!Directory.Exists(directoryPath))
+         {
+            // Ha nem létezik, hozza létre
+            Directory.CreateDirectory(directoryPath);
+             Console.WriteLine("Status: Config mappa inicializálás.");
+         }
+
+        // Ellenőrizze, hogy létezik-e a fájl
+        if (!File.Exists(filePath))
+        {
+        // Ha nem létezik, hozza létre
+             using (FileStream fs = File.Create(filePath))
+             Console.WriteLine("Status: Config file configurálás.");
+        }
+        else
+        {
+            Console.WriteLine("Status: Üdv újra minden rendben van.\n\n");
+        }
+    }
+
+    static void Help()
+    {
+        Console.WriteLine("|=============================|\n|Mappa hely megadas [1]       |\n|Kiterjesztes megadas [2]     |\n|Config File megjelenitese [3]|\n|Help [4]                     |\n|=============================|\n|Go [0]                       |\n|Exit [9]                     |\n|_____________________________|");
+    }
+
+
+    static void Menu()
+    {
+        Help();
+        while (true)
+        {
+            Console.Write(">>");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 0: go(); break;
+                case 1: add_spot(); break;
+                case 2: add_extension(); break;
+                case 3: print_config(); break;
+                case 4: Help(); break;
+                case 9: Console.WriteLine("Visszlat"); System.Environment.Exit(0); break;
+            }
+
+        }
+    }
     static void Main()
     {
-        string directoryPath = "C:\\test";
-        string extension = ".em";
-        string[] filepaths = Directory.GetFiles(directoryPath, "*" + extension, SearchOption.AllDirectories);
+            //init check
+            CheckAndCreateConfigFile();
 
-        Console.WriteLine(DirectoryOpen("C:\\test", ".em"));
-
-        SaveToPST(filepaths, "C:\\PST\\Output.pst");
-        Console.WriteLine("File mentve egy PST-be.");
-       
+            //Menu
+            Menu();
     }
 }
